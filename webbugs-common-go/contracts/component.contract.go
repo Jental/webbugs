2689b2e0eff0d1ffb7523b445e9813d74a9af26d
Nikolay Sokolov <nikolay@jamaza.com>
2021-07-06 11:44:34 +0300

    webbugs-common-go has been moved to a separate repo

--- webbugs-common-go/contracts/component.contract.go
+++
@@ -1,8 +0,0 @@
-package contracts
-
-// ComponentContract - contract for models.Component
-type ComponentContract struct {
-	ID       string                    `json:"id"`
-	IsActive bool                      `json:"isActive"`
-	WallIDs  []FullCoordinatesContract `json:"wall_ids"`
-}
--- webbugs-common-go/contracts/data.contract.go
+++
@@ -1,7 +0,0 @@
-package contracts
-
-// DataContract - contract for field data
-type DataContract struct {
-	Field      MultiFieldContract         `json:"field"`
-	Components map[uint]ComponentContract `json:"components"`
-}
